7b6f9d84f1e843016760673ccf7e55076666b20c
Gamze <gamzeogutcu@gmail.com>
2022-06-25 00:25:29 +0300

    CRUD operations are done.

---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class BookController : Controller
+    {
+        private static List<Book> BookList = new List<Book>()
+        {
+
+
+        new Book
+        {
+            Id = 1,
+            Title = "Lean Startup",
+            GenreId = 1, //Personal Growth
+            PageCount = 200,
+            PublishDate = new DateTime(2001,06,12)
+        },
+          new Book
+        {
+            Id = 2,
+            Title = "Herland",
+            GenreId = 2, //Science Fiction
+            PageCount = 250,
+            PublishDate = new DateTime(2010,05,23)
+        }, 
+            new Book
+           {
+            Id = 3,
+            Title = "Dune",
+            GenreId = 2, //Science Fiction
+            PageCount = 548,
+            PublishDate = new DateTime(2001,12,21)
+            },
+       };
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+            return bookList;
+        }
+       [HttpGet("{id}")]
+        public Book GetById(int id)
+        {
+            var book = BookList.Where(book => book.Id == id).SingleOrDefault();
+            return book;
+        }
+       /* [HttpGet]
+        public Book Get([FromQuery] string id)
+        {
+            var book = BookList.Where(book => book.Id == Convert.ToInt32(id)).SingleOrDefault();
+            return book;
+        }*/
+       //Post
+       [HttpPost]
+       public IActionResult AddBook([FromBody] Book newBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+            if (book != null)
+                return BadRequest();
+
+            BookList.Add(newBook);
+            return Ok();
+        }
+        //Put
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id,[FromBody] Book updatedBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+            if (book == null)
+                return BadRequest();
+            book.GenreId = updatedBook != default ? updatedBook.GenreId : book.GenreId;
+            book.PageCount = updatedBook != default ? updatedBook.PageCount : book.PageCount;
+            book.PublishDate = updatedBook != default ? updatedBook.PublishDate : book.PublishDate;
+            book.Title = updatedBook != default ? updatedBook.Title : book.Title;
+            return Ok();
+        }
+        
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+            if (book == null)
+                return BadRequest();
+            BookList.Remove(book);
+            return Ok();
+        }
+
+    }
+}
